8f44525091f4909d0f5150a1ca25ba944f3ac8d3
duswnwn <rita27@naver.com>
2023-09-25 17:25:11 +0900

    main + vote

--- goldbal/src/App.js
+++ goldbal/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-// import MainPage from './pages/MainPage.js';
-// import CreateGamePage from './pages/CreateGamePage.js';
+import MainPage from './pages/MainPage.js';
+import CreateGamePage from './pages/CreateGamePage.js';
 import VoteGamePage from './pages/VoteGamePage.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      {/* <MainPage />
+      <MainPage />
       <Routes>
         <Route path="/" exact component={MainPage} />
         <Route path="/new" component={CreateGamePage} />
-      </Routes> */}
-      <VoteGamePage />
+        <Route path="/vote" component={VoteGamePage} />
+      </Routes>
+      {/* <VoteGamePage /> */}
     </Router>
   );
 }
